9e5e6cb6e258bf9da53e66b70902d1b04e4cd1a4
artdengun <artdengun@gmail.com>
2021-01-09 00:54:10 +0700

    penambahan data

---
+++ src/Component/MenuItems.js
@@ -0,0 +1,37 @@
+export const MenuItems = [
+  {
+    title: "Sahabat AyoCepat",
+    path: "/sahabat-sicepat",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Linkedin",
+    path: "/linkedin",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Instagram",
+    path: "/instagram",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Facebook",
+    path: "/facebook",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Twitter",
+    path: "/twitter",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Youtube",
+    path: "/youtube",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Tiktok",
+    path: "/tiktok",
+    cName: "dropdown-link",
+  },
+];
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,11 +5,26 @@ import Dropdown from "./Dropdown";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   const handleStick = () => setClick(!click);
-
   const closeMobileMenu = () => setClick(false);
 
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
   return (
     <>
       <nav className="navbar">
